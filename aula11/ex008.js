6ffee02c2b90b4a95dbd044d6e2e74e72eb56538
kishinbr <viniciuskishibr@gmail.com>
2025-06-01 22:50:47 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log('O console funcionou corretament!')
+var velo = 59
+
+if(velo > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. Multado!`)
+
+}
+console.log(`A velocidade do seu carro é ${velo}km/h`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'a'
+if (pais == 'brasil'){
+    console.log(`Brasileiro`)
+}
+else{
+    console.log(`Estrangeiro`)
+}
